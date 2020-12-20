c22365b0af6cb68e48c7c6b956a6f1b09d3a54b1
ameryu <woodguiter28@gmail.com>
2020-12-21 05:43:47 +0900

    q

--- routes/pool.js
+++ routes/pool.js
@@ -6,7 +6,7 @@ const pool = new pg.Pool({
     password: process.env.key_pass,
     host: process.env.key_host,
     port: process.env.key_port,
-    ssl: false
+    ssl: true
 });
 
 module.exports = pool;
